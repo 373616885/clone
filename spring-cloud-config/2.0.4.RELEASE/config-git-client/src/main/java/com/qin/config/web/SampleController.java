8e88d1eb4178819c52813e26213947ced58eb4f5
qinjp <373616885@qq.com>
2018-09-05 18:06:32 +0800

    修改一些错误

--- spring-cloud-config/2.0.4.RELEASE/config-git-client/src/main/java/com/qin/config/web/SampleController.java
+++ spring-cloud-config/2.0.4.RELEASE/config-git-client/src/main/java/com/qin/config/web/SampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-    @Value("${name}")  // 从对应的配置中心找到文件并把属性注入到value值中
+    @Value("${name:123}")  // 从对应的配置中心找到文件并把属性注入到value值中
     private String value;
 
     @RequestMapping("/hello")
